2bc0b5986b73261b8d8ea9731ee5a764eb539869
Mahmoud Zouine <mzouine@e1r7p9.1337.ma>
2024-10-05 16:50:09 +0100

    auto push

--- mz_routine.c
+++ mz_routine.c
@@ -6,7 +6,7 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:46:10 by mzouine           #+#    #+#             */
-/*   Updated: 2024/10/05 14:29:24 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/10/05 14:48:03 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,9 @@ int	mz_check_death2(t_philo *philo)
 	pthread_mutex_lock(philo->death);
 	if (*(philo->is_dead) == 1)
 	{
+		pthread_mutex_unlock(&philo->fork_1->fork);
+		pthread_mutex_unlock(&philo->fork_2->fork);
+		pthread_mutex_unlock(philo->printer);
 		pthread_mutex_unlock(philo->death);
 		return (1);
 	}
@@ -55,21 +58,21 @@ int	mz_check_death2(t_philo *philo)
 int	mz_check_death(t_philo *philo)
 {
 	pthread_mutex_lock(philo->death);
-	if (*(philo->is_dead) == 1)
-	{
-		pthread_mutex_unlock(&philo->fork_1->fork);
-		pthread_mutex_unlock(&philo->fork_2->fork);
-		pthread_mutex_unlock(philo->printer);
-		pthread_mutex_unlock(philo->death);
-		return (1);
-	}
-	else if (get_time() - philo->last_meal >= philo->t_die || philo->meals_eaten >= philo->n_eat)
+	// if (*(philo->is_dead) == 1)
+	// {
+	// 	// pthread_mutex_unlock(&philo->fork_1->fork);
+	// 	// pthread_mutex_unlock(&philo->fork_2->fork);
+	// 	pthread_mutex_unlock(philo->printer);
+	// 	pthread_mutex_unlock(philo->death);
+	// 	return (1);
+	// }
+	if (get_time() - philo->last_meal >= philo->t_die || philo->meals_eaten >= philo->n_eat)
 	{
 		mz_printer(philo, 0);
 		*(philo->is_dead) = 1;
-		pthread_mutex_unlock(&philo->fork_1->fork);
-		pthread_mutex_unlock(&philo->fork_2->fork);
-		pthread_mutex_unlock(philo->printer);
+		// pthread_mutex_unlock(&philo->fork_1->fork);
+		// pthread_mutex_unlock(&philo->fork_2->fork);
+		// pthread_mutex_unlock(philo->printer);
 		pthread_mutex_unlock(philo->death);
 		return (1);
 	}
@@ -178,7 +181,7 @@ void *mz_routineMon(void *data)
 		{
 			if(mz_check_death(mon->philo[i]))
 			{
-				mz_funeral(mon);
+				// mz_funeral(mon);
 				return (NULL);
 			}
 			i++;
